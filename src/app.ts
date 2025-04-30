72dc51c10d3fb58725f817845396bb4f8703051e
Rafael Henrique da Silva <31450046+rafaelhalder@users.noreply.github.com>
2025-04-29 23:21:46 -0300

    feat: add CORS middleware to allow requests from localhost

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,12 @@ import express from "express";
 import { routes } from "./http/routes";
 import { errorHandlingMiddleware } from "./http/middlewares/error-handling";
 import cookieParser from "cookie-parser";
+import cors from "cors"; // Adicione esta linha
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(routes)
